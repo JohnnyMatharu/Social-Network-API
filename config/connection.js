631263cbda2a57c43b382c7b93c1a23df12e95c2
Johnny <jaani.matharu@gmail.com>
2021-06-21 20:59:00 -0400

    added basic structure

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+// Require Mongo ODM
+// MongoDB
+// Server.js
+
+
+//MOngo terminal is running (just type mongo it will run, if problem restart along with services and path command)
+// that is missing in module or documentation, your version is 4.2 not anything else)
+
+
+//You may also optionally use a JavaScript date library of your choice or the native JavaScript Date object to format timestamps.
+//Walkthrough video with link in readme and submitted seperate
+
+//Acceptance Criteria
+/*
+GIVEN a social network API
+WHEN I enter the command to invoke the application
+THEN my server is started and the Mongoose models are synced to the MongoDB database
+WHEN I open API GET routes in Insomnia Core for users and thoughts
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete users and thoughts in my database
+WHEN I test API POST and DELETE routes in Insomnia Core
+THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list
+*/
+
+
+
+//Hilary demo: everything in the video
+//user, thought and reaction as models and friends as sub-document 
+
+//current MOck up on assignment page, match file structure with mod code 
